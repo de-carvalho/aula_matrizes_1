9c01017030acb5f47324684549cd87ac8653ca37
Ana Carvalho <anacarvalho@MacBook-Pro-de-Ana.local>
2025-06-13 20:31:25 -0300

    Atualizando exercicio 10

--- 10_exercicio.py
+++ 10_exercicio.py
@@ -1,16 +1,19 @@
-# Escreva um programa em python que gere uma matriz M[10][10] com números aleatórios entre 1 e 50
-# Peça um número de uma linha qualquer, entre 0 e 9 e copie os elementros dessa linha para um vetor
-# Imprima a matriz e o vetor
-
-from random import randint
+import random
 
 matriz = []
 for i in range(10):
     linha = []
     for j in range(10):
-        valor = randint(1, 50)
-        linha.append(valor)
+        linha.append(random.randint(1, 50))
     matriz.append(linha)
+
 print("Matriz:")
 for linha in matriz:
-    print(linha) 
+    print(linha)
+
+numero_linha = int(input("Escolha uma linha (0 a 9): "))
+
+vetor = matriz[numero_linha]
+
+print("Vetor com os números da linha escolhida:")
+print(vetor)
